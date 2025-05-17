66aa9fbbfde9281c36e5996c674f4f40e98b51dc
dhairyasonix <sonidhairya160@gmail.com>
2025-05-17 21:20:15 +0530

    add responsive to bkingexpert

--- src/Components/BakingExperts.jsx
+++ src/Components/BakingExperts.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const BakingExperts = () => {
   return (
-    <div id="Story" className="flex flex-col px-36 py-[12vh] items-center text-center ">
-      <div className="w-1/2 ">
-        <p className="text-lg font-semibold my-2">Our Chef</p>
-        <h2 className="text-4xl md:text-4xl font-bold text-gray-800 mb-4">
+    <div id="Story" className="flex flex-col px-4 md:px-36 py-[12vh] md:items-center text-start md:text-center ">
+      <div className="md:w-1/2 w-[70%] mx-auto">
+        <p className="text-md md:text-lg font-semibold my-1">Our Chef</p>
+        <h2 className="text-2xl md:text-4xl font-bold text-gray-800 mb-4">
           Meet our awesome Chefs
         </h2>
         <p className="text-gray-600 mb-6">
